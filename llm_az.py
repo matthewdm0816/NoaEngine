55341120f321148c730cc410d3693f5b07e2523b
UEFI-code <1092700291@qq.com>
2023-09-30 12:36:27 +0800

    Add Azure OAI Support

---
+++ llm_az.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+class ChatGPT3_5:
+    def __init__(self, endpoint, apiKey):
+        self.endpoint = endpoint
+        self.header = {
+            "Content-Type": "application/json",
+            "api-key": apiKey
+        }
+        self.messages = [{"role":"system","content":"You are a smart robot girl."}]
+        self.body = {
+            "messages": None,
+            "max_tokens": 256,
+            "temperature": 0.7,
+            "top_p": 1,
+            "frequency_penalty": 0,
+            "presence_penalty": 0,
+            "stop": None
+        }
+    
+    def forward(self, x = 'Hello World'):
+        self.body['messages'] = self.messages + [{"role":"user","content":x}]
+        self.body['max_tokens'] = 3900 - len(x)
+        if (self.body['max_tokens'] < 0):
+            raise Exception('The input text is too long.')
+        response = requests.post(self.endpoint, headers=self.header, data=json.dumps(self.body))
+        return response.json()['choices'][0]['message']['content']
